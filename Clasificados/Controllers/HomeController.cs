82ccac83fa9bf43e72089cef76e259a803b34fc3
armejiaf <mejia561@hotmail.com>
2014-10-30 18:35:41 -0600

    Clasificados1

---
+++ Clasificados/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clasificados.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult RecoverPassword()
+        {
+            return View();
+        }
+    }
+}
